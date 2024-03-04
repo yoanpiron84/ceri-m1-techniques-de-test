e4776dfc4dc8d494e43b7b173de58d27b9fcbb6e
yoanpiron <yoan.piron@alumni.univ-avignon.fr>
2024-03-04 11:30:07 +0100

    Commit pokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -6,23 +6,36 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import fr.univavignon.pokedex.api.IPokemonFactory;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public abstract class IPokemonFactoryTest {
+public class IPokemonFactoryTest {
 
     @Mock
     public IPokemonFactory pokemonFactory;
 
     @BeforeEach
     public void setUp() throws PokedexException {
-        when(pokemonFactory.createPokemon(0,613,64,4000,4)).thenReturn(new Pokemon(0,"Bulbizarre",126,126,90,613,64,4000,4,56));
+        when(pokemonFactory.createPokemon(0, 613, 64, 4000, 4))
+                .thenReturn(new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56));
+
+        // Configuration du comportement du mock pour lancer une exception lorsque l'index est négatif
+        when(pokemonFactory.createPokemon(-1, 613, 64, 4000, 4))
+                .thenThrow(new PokedexException("Index négatif !"));
     }
 
     @Test
-    public void shouldReturnNegatifWhenCPNegatif() throws PokedexException {
-        when(pokemonFactory.createPokemon(0,-1,64,4000,4)).thenThrow(new PokedexException("CP Négatif"));
-        PokemonMetadata pokemonMetadata = pokemonFactory.createPokemon(0,-1,64,4000,4);
+    public void shouldThrowExceptionWhenIndexNegative() {
+        assertThrows(PokedexException.class,
+                () -> pokemonFactory.createPokemon(-1, 613, 64, 4000, 4));
+    }
+
+    @Test
+    public void shouldReturnPokemonName(){
+        assertEquals("Bulbizarre", pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getName());
     }
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,30 +1,43 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.assertEquals;
+
 import fr.univavignon.pokedex.api.IPokemonMetadataProvider;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class IPokemonMetadataProviderTest {
 
     @Mock
-    public IPokemonMetadataProvider pokemonMetadataProvider;
+    private IPokemonMetadataProvider pokemonMetadataProvider;
+    private PokemonMetadata validMetadata;
 
     @BeforeEach
     public void setUp() throws PokedexException {
-        when(pokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0,"Bulbizarre",126,126,90));
+        pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        validMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        Mockito.when(pokemonMetadataProvider.getPokemonMetadata(-1)).thenThrow(new PokedexException("Index invalide !"));
     }
 
     @Test
-    public void shouldReturnBulbizarreWhen0() throws PokedexException {
-        assertEquals("Bulbizarre",pokemonMetadataProvider.getPokemonMetadata(0).getName());
+    public void shouldReturnCorrectPokemon() throws PokedexException {
+        PokemonMetadata pokemonMetadataCorrect = pokemonMetadataProvider.getPokemonMetadata(0);
+        assertEquals(validMetadata, pokemonMetadataCorrect);
+    }
+
+    @Test
+    public void shouldThrowWhenInvalidIndex() throws PokedexException {
+        PokemonMetadata actualMetadata = pokemonMetadataProvider.getPokemonMetadata(-1);
+        assertEquals(null, actualMetadata);
     }
 
 }
