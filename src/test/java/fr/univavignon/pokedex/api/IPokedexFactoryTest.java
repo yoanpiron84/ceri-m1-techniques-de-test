33826b93ca665524b27ea86f075050b01bb117f0
yoanpiron <yoan.piron@alumni.univ-avignon.fr>
2024-03-18 11:10:30 +0100

    Changement mock par vraie instance Pokedex

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -6,26 +6,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class IPokedexFactoryTest {
-    private IPokedexFactory pokedexFactory;
+    private PokedexFactory pokedexFactory;
     private IPokemonMetadataProvider metadataProvider;
     private IPokemonFactory pokemonFactory;
 
     @Before
     public void setUp() throws PokedexException {
-        pokedexFactory = mock(IPokedexFactory.class);
         metadataProvider = mock(IPokemonMetadataProvider.class);
         pokemonFactory = mock(IPokemonFactory.class);
-        IPokedex pokedex = mock(IPokedex.class);
-        when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
+        pokedexFactory = new PokedexFactory();
     }
 
     @Test
     public void testCreatePokedex() throws PokedexException {
+        // Créer un pokedex à l'aide de la factory
+        IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
 
+        // Vérifier que le pokedex a été créé avec succès
+        assertNotNull(pokedex);
     }
 }
-
